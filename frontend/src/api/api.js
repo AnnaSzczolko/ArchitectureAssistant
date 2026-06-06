19af34d05784a79c63481bd9c2bedfa080fbbdc8
Ania <anna.szczolko@gmail.com>
2026-06-06 17:40:16 +0200

    check analyze response

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,22 +1,25 @@
 // const BASE = "http://localhost:3000";
-const BASE = "https://architectureassistant.onrender.com";
+const BASE = 'https://architectureassistant.onrender.com'
 
 export const getParcels = async () => {
-  const res = await fetch(`${BASE}/parcels`);
-  return res.json();
-};
+	const res = await fetch(`${BASE}/parcels`)
+	return res.json()
+}
 
 export const getRequests = async () => {
-  const res = await fetch(`${BASE}/projects`);
-  return res.json();
-};
+	const res = await fetch(`${BASE}/projects`)
+	return res.json()
+}
 
 export const analyze = async (parcelId, requestId) => {
-  const res = await fetch(`${BASE}/analysis`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ parcelId, requestId }),
-  });
+	const res = await fetch(`${BASE}/analysis`, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ parcelId, requestId }),
+	})
 
-  return res.json();
-};
+	const data = await res.json()
+	console.log('ANALYSIS RESPONSE:', data)
+
+	return res.json()
+}
